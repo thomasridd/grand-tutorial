066930af93406eef62e436d1a967e50d3ca638de
Tom Ridd <twridd@gmail.com>
2018-11-10 15:41:49 +0000

    Fix import into Homepage/index.js

--- src/scenes/Homepage/index.js
+++ src/scenes/Homepage/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './styles.scss'
-import Banner from './components/Banner'
+import Banner from '../../components/Banner'
 import BodySection from './components/BodySection'
 import HeaderSection from './components/HeaderSection'
-import FooterSection from './components/FooterSection'
+import FooterSection from '../../components/FooterSection'
 
 export default (props) => {
     return (
